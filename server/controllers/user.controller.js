659ef69a06b00ede50c62e75227068823fcbe3d0
MrBreakIT <johnpike1022@gmail.com>
2020-09-21 15:43:00 -0500

    updated user model w- message model

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,31 +1,32 @@
 const User = require('../models/user.model');
+const Messages = require('../models/user.model');
 
 module.exports = {
-    index : (req,res) => {
+    index: (req, res) => {
         User.find()
-            .then(user =>{
-                res.json({results:user})
+            .then(user => {
+                res.json({ results: user })
             })
             .catch(err => {
                 console.log(err);
             })
     },
-    create : (req,res) => {
+    create: (req, res) => {
         User.create(req.body)
             .then(user => {
-                res.json({results:user})
+                res.json({ results: user })
             })
             .catch(err => res.json(err.errors))
     },
-    show : (req,res) => {
-        user.findOne({_id:req.params.id})
+    show: (req, res) => {
+        User.findOne({ _id: req.params.id })
             .then(user => {
-                res.json({results:user})
+                res.json({ results: user })
             })
             .catch(err => res.json(err.errors))
     },
-    update : (req,res) => {
-        user.findOne({_id: req.params.id}, function(err,user){
+    update: (req, res) => {
+        User.findOne({ _id: req.params.id }, function (err, user) {
             user.userFirstName = req.body.userFirstName;
             user.userLastName = req.body.userLastName;
             user.userEmail = req.body.userEmail;
@@ -34,18 +35,18 @@ module.exports = {
             user.userBranch = req.body.userBranch;
             user.userRank = req.body.userRank;
             user.userRateMOS = req.body.userRateMOS;
-            user.userMessage = req.body.userMessage;
-            user.save(function(err,saveduser) {
-                if(saveduser == null)
+            user.userLOS = req.body.userMessage;
+            user.save(function (err, saveduser) {
+                if (saveduser == null)
                     res.status(400).json(err);
-                else 
-                    res.json({user:savedUser});
+                else
+                    res.json({ user: savedUser });
             })
         })
     },
-    destroy : (req,res) => {
-        user.deleteOne({_id:req.params.id})
-            .then(result => res.json({results: result}))
+    destroy: (req, res) => {
+        User.deleteOne({ _id: req.params.id })
+            .then(result => res.json({ results: result }))
             .catch(err => res.json(err.errors))
     }
 }
