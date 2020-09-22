b3c1787cfb3edd9fb655c97ede6529ee7a669009
MrBreakIT <johnpike1022@gmail.com>
2020-09-22 16:55:26 -0500

    working on log/reg-JWT

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
 
 
 const MessageSchema = new mongoose.Schema({
@@ -14,27 +17,31 @@ const MessageSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
     userFirstName: {
         type: String,
-        required: [true, "Must enter user First Name this time"],
+        required: [true, "First Name required"],
         minlength: [2, "name has to be more than 2 characters"]
     },
     userLastName: {
         type: String,
-        required: [true, "Must enter user Last Name"],
+        required: [true, "Last Name required"],
         minlength: [2, "name has to be more than 2 characters"]
     },
     userName: {
         type: String,
-        required: [true, "Must enter a username"],
+        required: [true, "Username required"],
         minlength: [5, "type need to be at least 5 characters"]
     },
     userEmail: {
         type: String,
-        required: [true, "Must enter a valid email"],
-        // minlength: [3,"type need to ba at least 3 characters"]
+        required: [true, "Email required"],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
+
     },
     userPassword: {
         type: String,
-        required: [true, "Password"],
+        required: [true, "Password required"],
         minlength: [6, "Password longer than 5 characters"]
     },
     userConfirmPW: {
@@ -73,6 +80,33 @@ const UserSchema = new mongoose.Schema({
 
 }, { timestamps: true })
 
+// add this after UserSchema is defined
+UserSchema.virtual('confirmPassword')
+    .get(() => this.confirmPassword)
+    .set(value => this.confirmPassword = value);
+
+UserSchema.pre('validate', function (next) {
+    if (this.userPassword !== this.userConfirmPW) {
+        this.invalidate('userConfirmPW', 'Password must match confirm password');
+    }
+    next();
+});
+// bcrypt userPassword
+UserSchema.pre('save', function (next) {
+    bcrypt.hash(this.userPassword, 10)
+        .then(hash => {
+            this.userPassword = hash;
+            next();
+        });
+});
+// bcrypt user confirmPW
+UserSchema.pre('save', function (next) {
+    bcrypt.hash(this.userConfirmPW, 10)
+        .then(hash => {
+            this.userConfirmPW = hash;
+            next();
+        });
+});
 
 
 //this is how we register our schema.
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,10 +3,11 @@ const UserController = require('../controllers/user.controller');
 
 module.exports = (app) => {
     app.get('/api/user', UserController.index);
-    app.post('/api/create/user', UserController.create);
+    app.post('/api/create/user', UserController.register);
     app.get('/api/user/:id', UserController.show);
     app.put('/api/update/user/:id', UserController.update);
     app.delete('/api/destroy/user/:id', UserController.destroy);
+
 }
 
 
