56fee0b67a15da79b554ab8e46f7127745239d01
Robert Santos <rasbms81608@gmail.com>
2020-09-23 12:27:28 -0500

    some front in styling

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,18 @@ import AllUsers from './views/AllUsers'
 import ShowUser from './views/ShowUser'
 // import Show from './views/Show';
 // import Edit from './views/Edit';
+import background from './assets/img/military.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <div className="col text-align-end">
+    <div className="App" style={{backgroundImage:` url(${background})`}}>
+      <div className="col-10 mx-auto text-align-end bg-white">
         userName/ length of service/ mos
       </div>
       <div className="col mx-auto">
-        <Link to="/new/"><h1 style={{color:"black"}}>Military Facebook</h1></Link>
+        <Link to="/new/"><h1 style={{color:"black"}} className="bg-white">Military Facebook</h1></Link>
       </div>
-      <div className="col">
+      <div className="col bg-white">
         {/* <Link to="/new" className="btn btn-info btn-outline-dark">Military Facebook</Link> */}
         <Link to="/" className="btn btn-info btn-outline-dark">Home</Link>
         <Link to="/user" className="btn btn-info btn-outline-dark">Friends</Link>
